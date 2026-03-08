6a151b3a4a7067578f3a3ba422813547519e407e
mdx1230 <ii1452671@gmail.com>
2026-03-08 16:08:38 +0300

    interfaces

---
+++ CartItem.ts
@@ -0,0 +1,8 @@
+export interface CartItem {
+
+    productId: string
+    title: string
+    price: number
+    quantity: number
+
+}
---
+++ User.ts
@@ -0,0 +1,9 @@
+export interface User {
+
+    name: string
+    email: string
+    login: string
+    phone: string
+    password: string
+
+}
