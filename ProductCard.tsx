b733be2d006ec268bcefee04b85ce2519fe421d2
mdx1230 <ii1452671@gmail.com>
2026-03-08 17:42:16 +0300

    Update ProductCard.tsx

--- ProductCard.tsx
+++ ProductCard.tsx
@@ -1,34 +1,23 @@
+import { useContext } from "react"
+import { CartContext } from "../store/CartContext"
 import { Product } from "../types/Product"
 
 interface Props {
-
     product: Product
-    onAdd: (id: string) => void
-
 }
 
-export default function ProductCard({ product, onAdd }: Props) {
+export default function ProductCard({ product }: Props) {
+
+    const { addToCart } = useContext(CartContext)
 
     return (
-
         <div className="product-card">
-
-            <h3 data-title>
-                {product.name}
-            </h3>
-
+            <h3 data-title>{product.name}</h3>
             <p>{product.description}</p>
-
-            <span data-price>
-                {product.price}
-            </span>
-
-            <button onClick={() => onAdd(product.id)}>
-                Add to cart
+            <span data-price>${product.price}</span>
+            <button onClick={() => addToCart(product)}>
+                Добавить в корзину
             </button>
-
         </div>
-
     )
-
 }
