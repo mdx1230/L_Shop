a9a562cf9ba7adfda6b3c376cd766903090c72db
mdx1230 <ii1452671@gmail.com>
2026-03-09 12:33:15 +0300

    Add files via upload

---
+++ AppRouter.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "../pages/HomePage"
+import LoginPage from "../pages/LoginPage"
+
+export default function AppRouter() {
+
+    return (
+
+        <BrowserRouter>
+
+            <Routes>
+
+                <Route
+                    path="/"
+                    element={<HomePage />}
+                />
+
+                <Route
+                    path="/login"
+                    element={<LoginPage />}
+                />
+
+            </Routes>
+
+        </BrowserRouter>
+
+    )
+
+}
